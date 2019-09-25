28839ed95c50171ad6f4fdd037210090436ed65c
Nathan <nathanyoung@billericak12.com>
2019-09-25 11:30:20 -0400

    mhm

--- src/com/company/MathEx.java
+++ src/com/company/MathEx.java
@@ -1,4 +1,16 @@
 package com.company;
 
 public class MathEx {
+
+    public static int swap(int x){
+        int y = x % 10;
+        int a = x % 100 / 10;
+        int b = x / 100;
+        int c = b * 100 + y * 10 + a;
+        return c;
+    }
+    public static void main(String [] args) {
+        int z = swap(123);
+        System.out.print(z);
+    }
 }
